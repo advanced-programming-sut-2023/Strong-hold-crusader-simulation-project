7ff197d79d7d240d67eab3e4676bef762bbdc83b
DarkKnight <faridmahmoudzadeh@gmail.com>
2023-04-19 20:16:52 +0430

    :)

--- view/commands/StoreMenuCommands.java
+++ view/commands/StoreMenuCommands.java
@@ -1,4 +1,19 @@
 package view.commands;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public enum StoreMenuCommands {
+    ;
+    private final String regex;
+
+    StoreMenuCommands(String regex) {
+        this.regex = regex;
+    }
+
+    public static Matcher getMatcher(String input, StoreMenuCommands command) {
+        Matcher matcher = Pattern.compile(command.regex).matcher(input);
+        if(matcher.matches()) return matcher;
+        else return null;
+    }
 }
