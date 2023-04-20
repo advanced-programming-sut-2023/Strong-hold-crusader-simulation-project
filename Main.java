5c90743f5d95f5618759e403663df28a8f4d0529
DarkKnight <faridmahmoudzadeh@gmail.com>
2023-04-20 20:59:51 +0430

    added login

--- Main.java
+++ Main.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        User.getAllUsers().add(new User("Salam","password"));
         Scanner scanner = new Scanner(System.in);
         LoginMenu loginMenu = new LoginMenu(scanner);
         loginMenu.run();
